4b6a1b2c5cfecc4433a66c02ffb627c530691a48
Олег <olegchuiko6@gmail.com>
2024-03-23 00:44:50 +0200

    add WhiteNoise

--- expresstest/settings.py
+++ expresstest/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -154,6 +155,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
     ]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
