542319b6fa0bee0aa91f59a1223deb14f94afaa1
Hammerszy <115892314+Hammerszy@users.noreply.github.com>
2024-04-18 16:25:23 +0300

    add newPasswordResetFormPage, newPasswordResetDonePage, newPasswordResetConfirmPage, newPasswordResetCompletePage, menuConfirmDeleteTest, fixSomeBugs

--- staticfiles/js/dashboard/confirmDeleteScript.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.menu-test-view').forEach(button => {
-    button.addEventListener('click', function() {
-        const testId = this.closest('.edit-buttons').querySelector('input[name="test_id"]').value;
-        if (confirm('Ви впевнені, що хочете видалити цей тест?')) {
-            this.closest('.menu-test-view').submit();
-        }
-    });
-});
